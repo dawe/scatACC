bfa43b0dc2587464f1f798011db8402294e74e74
Cittaro Davide <cittaro.davide@gmail.com>
2021-11-23 10:30:30 +0100

    added a script to add CB from RG

---
+++ CBfromRG.py
@@ -0,0 +1,28 @@
+import sys
+import pysam
+
+if len(sys.argv) < 3:
+    print("You should specify BAM in and BAM out")
+    sys.exit(1)
+
+fin = sys.argv[1]
+fout = sys.argv[2]
+
+bam_in = pysam.AlignmentFile(fin, 'rb')
+in_header = bam_in.header
+
+bam_out = pysam.AlignmentFile(fout, 'wb', header=in_header)
+rg_sm = {}
+
+for record in in_header['RG']:
+    rg_sm[record['ID']] = record['SM']
+
+for record in bam_in:
+    tags = record.tags.copy()
+    rg = [[x[1] for x in tags if x[0]=='RG'][0]
+    tags.append(('CB', rg_sm[rg]))
+    record.tags = tags
+    bam_out.write(record)
+
+bam_in.close()
+bam_out.close()    
