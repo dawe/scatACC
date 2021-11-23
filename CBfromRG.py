209f64adbc56315270c17816a59b50ce3f2f07f3
Cittaro Davide <cittaro.davide@gmail.com>
2021-11-23 10:34:26 +0100

    do not process background

--- CBfromRG.py
+++ CBfromRG.py
@@ -20,8 +20,10 @@ for record in in_header['RG']:
 for record in bam_in:
     tags = record.tags.copy()
     rg = [x[1] for x in tags if x[0]=='RG'][0]
-    tags.append(('CB', rg_sm[rg]))
-    record.tags = tags
+    sm = rg_sm[rg]
+    if sm != 'Background':
+	    tags.append(('CB', rg_sm[rg]))
+	    record.tags = tags
     bam_out.write(record)
 
 bam_in.close()
