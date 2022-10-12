1c8ed94f90b628f5cb6157e28d523b56c375880b
dawe <daweonline@gmail.com>
2022-10-12 15:13:08 +0200

    add dup stats to cbdedup

--- cbdedup.py
+++ cbdedup.py
@@ -1,6 +1,9 @@
 import pysam
 import argparse
 import sys
+from collections import Counter
+from natsort import natsorted
+import os
 
 def get_options():
   parser = argparse.ArgumentParser(prog='cbdedup.py')
@@ -39,30 +42,39 @@ def main():
     bam_in = pysam.AlignmentFile(options.bamfile, 'rb')
 
   if options.stdout:
+    stat_file = 'stdout_dup_stats.txt'
     options.output = sys.stdout
+  else:
+    stat_file = options.output.replace('.bam', '') + '_dup_stats.txt'
 
   bam_out = pysam.AlignmentFile(options.output, 'wb', template=bam_in)
 
   buffer = []
   hash_in_buffer = set()
+  hash_counter = Counter()
 
   for alignment in bam_in:
     alignment.is_duplicate = False #undedup if any
     h_ali = hash_read(alignment)
-    if 	h_ali in hash_in_buffer and not alignment.is_unmapped:
-  	  alignment.is_duplicate = True
+    if h_ali in hash_in_buffer and not alignment.is_unmapped:
+          alignment.is_duplicate = True
+          hash_counter.update([h_ali]) 
     hash_in_buffer.add(h_ali)
     buffer.append(alignment)
     if len(buffer) == options.buffer:
       for ali_out in buffer:
         bam_out.write(ali_out)
       buffer = []
-      hash_in_buffer = set()	
+      hash_in_buffer = set()
 
   #clean buffer at the end
   for ali_out in buffer:
     bam_out.write(ali_out)
 
+  dup_count = Counter(hash_counter.values())
+  with open(stat_file, 'w') as fout:
+    for k in natsorted(dup_count.keys()):
+      fout.write(f"{k}\t{dup_count[k]}\n")
 
   bam_out.close()
   bam_in.close()
