e0f6188ed2380d3b444b8412c554a68df13f67e6
Cittaro Davide <cittaro.davide@gmail.com>
2023-02-28 10:07:47 +0100

    threshold by bc

--- merge_share_bc.py
+++ merge_share_bc.py
@@ -18,7 +18,7 @@ def get_options():
   parser = argparse.ArgumentParser(prog='merge_share_bc.py')
   parser.add_argument('-b', '--read_bc', help='Read containing cell barcode (R2)')
   parser.add_argument('-u', '--read_umi', help='Read containing UMI (R3, RNA only)')
-  parser.add_argument('-e', '--max_err', help='Max edit distance', default=3, type=int)
+  parser.add_argument('-e', '--max_err', help='Max edit distance (comma separated)', nargs=3, default=[1, 1, 1])
   parser.add_argument('-d', '--debug', help='Print debug information', action='store_true')
 #  parser.add_argument('-o', '--output', help='Prefix for outputfile (in pickle format)')
 #  parser.add_argument('-E', '--threshold', help='General threshold for edit distance', default=1, type=int)
@@ -29,6 +29,8 @@ def get_options():
 
 def main():
     options = get_options()
+    
+    thrs = [int(x) for x in options.max_err]
     
     sp1 = 'TCGGACGATCATGGG' # [0:15]
     sp2 = 'CAAGTATGCAGCGCGCTCAAGCACGTGGAT' # [23:53]
@@ -55,7 +57,7 @@ def main():
         e3 = ed.eval(bc_s[61:91], sp3)
         if options.debug:
             sys.stderr.write(f'{e1}\t{e2}\t{e3}\n')
-        if e1 + e2 + e3 > options.max_err:
+        if e1 > thr[0] and e2 > thr[1] and e3 > thr[2]:
             continue
         comb_bc = bc_s[15:23] + bc_s[53:61] + bc_s[91:99]
         comb_ql = bc_q[15:23] + bc_q[53:61] + bc_q[91:99]
